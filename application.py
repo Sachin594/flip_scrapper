21468068cacd9006a7bace5b718f3bb83d7118cf
Sachin594 <120539780+Sachin594@users.noreply.github.com>
2024-02-27 17:20:51 +0530

    Update application.py

--- application.py
+++ application.py
@@ -71,7 +71,7 @@ def index():
                 reviews.append(mydict)
             
                      
-            return render_template('result.html',reviews=reviews[0:len(reviews)-1])
+            return render_template('results.html',reviews=reviews[0:len(reviews)-1])
 
             
             
