c4e97cf5bb582dd268c6d4e7e87b420bbbafa3ef
Sachin594 <pandasachin947@gmail.com>
2024-02-27 16:09:53 +0530

    first commit

--- application.py
+++ application.py
@@ -104,7 +104,7 @@ def index():
         return render_template('index.html')
   
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0')
 	#app.run(debug=True)
         
     
